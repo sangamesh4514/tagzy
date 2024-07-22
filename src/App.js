7eb8208f8ea228a86709bb4f2a8584e65ed735a2
Nawodit Nayan <38028042+nawodit@users.noreply.github.com>
2024-07-22 16:50:24 +0000

    comment-changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Comingsoon2 from './pages/Comingsoon2'
-import NewPage from './components/NewPage'
+// import NewPage from './components/NewPage'
 import Greet from './pages/Greet'
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Comingsoon2 />} />
-          <Route path='/newpage' element={<NewPage />} />
+          {/* <Route path='/newpage' element={<NewPage />} /> */}
           <Route path='/greet' element={<Greet />} />
         </Routes>
       </BrowserRouter>
