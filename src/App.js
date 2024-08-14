8e71052b4633798eb69757ac8ad9dce3877d5b34
Nawodit Nayan <38028042+nawodit@users.noreply.github.com>
2024-08-14 12:25:31 +0000

    add-new-page-content333

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Comingsoon2 from './pages/Comingsoon2'
+import Comingsoon from './pages/Comingsoon'
 import NewPage from './components/NewPage'
 import Greet from './pages/Greet'
 const App = () => {
@@ -8,8 +8,8 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Comingsoon2 />} />
-          <Route path='/newpage' element={<NewPage />} />
+          <Route path='/' element={<Comingsoon />} />
+          <Route path='/new' element={<NewPage />} />
           <Route path='/greet' element={<Greet />} />
         </Routes>
       </BrowserRouter>
