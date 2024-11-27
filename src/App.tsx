6a1b20ff344a20a20acbd96a8fbc88c0cd547cd5
nawodit.nayan <nawodit.nayan@telekom-digital.com>
2024-11-27 09:11:26 +0530

    ticket-56-not-found-page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './common/routes/Home'
 import Dashboard from './common/routes/Dashboard'
 import ProtectedRoute from './common/routes/ProtectedRoute'
 import ProProfile from './common/components/profile/userProfile'
+import NotFound from './common/routes/NotFound'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path='/privacyPolicy' element={<PrivacyPolicy />} />
           <Route path='/admin/login' element={<AdminAuth />} />
           <Route path='/profile/:userId' element={<ProProfile />} /> 
+          <Route path="*" element={<NotFound />} />
 
           {/* Protected Routes */}
           <Route path='/' element={<ProtectedRoute />} >
