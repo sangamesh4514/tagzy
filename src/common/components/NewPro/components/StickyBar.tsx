1ba88e377bacecd2fa5df04f305f3cd7b2b98ce3
nawodit.nayan <nawodit.nayan@telekom-digital.com>
2024-12-31 19:45:43 +0530

    day-31

--- src/common/components/NewPro/components/StickyBar.tsx
+++ src/common/components/NewPro/components/StickyBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import "../styles/StickyBar.css";
 import { useCart } from "../context/CartContext";
